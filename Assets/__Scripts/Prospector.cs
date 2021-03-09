85919ced84025297d3c0222c6e258567d484af08
QinyangCai224 <qc76@scarletmail.edu>
2021-03-09 19:52:30 +0800

    replace card script

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -5,34 +5,53 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class Prospector : MonoBehaviour {
+public class Prospector : MonoBehaviour
+{
 
-	static public Prospector 	S;
+	static public Prospector S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
-	public TextAsset			layoutXML;
+	public TextAsset deckXML;
+	public TextAsset layoutXML;
 
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
-	public Layout				layout;
+	public Deck deck;
+	public Layout layout;
+	public List<CardProspector> drawPile;
 
-	void Awake(){
+	void Awake()
+	{
 		S = this;// Set up a Singleton for Prospector
 	}
 
-	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
+	void Start()
+	{
+		deck = GetComponent<Deck>();
+		deck.InitDeck(deckXML.text);
 		Deck.Shuffle(ref deck.cards);
 		// This section can be commented out; we're working on real layout now
-		// Card c;
-		// for (int cNum=0; cNum<deck.cards.Count; cNum++) {
-		// c = deck.cards[cNum];
-		// c.transform.localPosition = new Vector3((cNum%13)*3,cNum/13*4,0);
-		// }
+		//Card c;
+		//for (int cNum=0; cNum<deck.cards.Count; cNum++) {
+		//c = deck.cards[cNum];
+		//c.transform.localPosition = new Vector3((cNum%13)*3,cNum/13*4,0);
+		//}
 		layout = GetComponent<Layout>(); // Get the Layout component
 		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+
+		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+	}
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+	{
+		List<CardProspector> lCP = new List<CardProspector>();
+		CardProspector tCP;
+		foreach (Card tCD in lCD)
+		{
+			tCP = tCD as CardProspector; // a
+			lCP.Add(tCP);
+		}
+		return (lCP);
+
 	}
 }
+
