abbcaf8a699681305461625b35fd28c8f2b72295
QinyangCai224 <qc76@scarletmail.edu>
2021-03-09 17:52:29 +0800

    layout

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -11,18 +11,28 @@ public class Prospector : MonoBehaviour {
 
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
+	public TextAsset			layoutXML;
 
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
+	public Layout				layout;
 
 	void Awake(){
-		S = this;
+		S = this;// Set up a Singleton for Prospector
 	}
 
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
+		Deck.Shuffle(ref deck.cards);
+		// This section can be commented out; we're working on real layout now
+		// Card c;
+		// for (int cNum=0; cNum<deck.cards.Count; cNum++) {
+		// c = deck.cards[cNum];
+		// c.transform.localPosition = new Vector3((cNum%13)*3,cNum/13*4,0);
+		// }
+		layout = GetComponent<Layout>(); // Get the Layout component
+		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
 	}
-
 }
