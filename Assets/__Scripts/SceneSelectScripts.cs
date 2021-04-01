5e54c4b8baca15806449fe50d261dfb334a430fd
QinyangCai224 <qc76@scarletmail.edu>
2021-04-01 13:59:51 +0800

    main scene

---
+++ Assets/__Scripts/SceneSelectScripts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectScripts : MonoBehaviour
+{
+    public void selectSene() {
+        switch (this.gameObject.name) {
+        case"__Prospector_Scene_0Button":
+                SceneManager.LoadScene("__Prospector_Scene_0");
+            break;
+        case "MyGameButton":
+                SceneManager.LoadScene("MyGame");
+            break;
+    }
+
+}
+
+    }
